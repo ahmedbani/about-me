835fa354889681bd8bb1be2e77ca29506863a09d
ahmedbani <a13banisal@gmail.com>
2021-07-06 15:29:15 +0300

    add 2 more questions to the website and used in the country question 2 loops to check the answer with the array

--- js/app.js
+++ js/app.js
@@ -10,18 +10,18 @@ alert('lets play a guessing game');
 let answer;
 function checkTrue(a) {
     a = a.toLowerCase();
-    if (a == 'yes' || a=='y') {
+    if (a == 'yes' || a == 'y') {
         //console.log('correct');
         alert('correct');
-    } else if (a == 'no' || a== 'n') {
+    } else if (a == 'no' || a == 'n') {
         //console.log('wrong');
         alert('wrong')
-        
+
     } else {
-       
-        while(a != 'yes' && a!='y' && a!='no' && a!='n'){
+
+        while (a != 'yes' && a != 'y' && a != 'no' && a != 'n') {
             //console.log('please answer with yes/y or no/n');
-            a=prompt('please answer with yes/y or no/n');
+            a = prompt('please answer with yes/y or no/n');
 
         }
         checkTrue(a);
@@ -30,16 +30,16 @@ function checkTrue(a) {
 }
 function checkFalse(a) {
     a = a.toLowerCase();
-    if (a == 'yes' || a=='y') {
+    if (a == 'yes' || a == 'y') {
         //console.log('wrong');
         alert('wrong');
-    } else if (a == 'no' || a== 'n') {
+    } else if (a == 'no' || a == 'n') {
         //console.log('correct');
         alert('correct')
     } else {
-        while(a != 'yes' && a!='y' && a!='no' && a!='n'){
+        while (a != 'yes' && a != 'y' && a != 'no' && a != 'n') {
             //console.log('please answer with yes/y or no/n');
-            a=prompt('please answer with yes/y or no/n');
+            a = prompt('please answer with yes/y or no/n');
 
         }
         checkFalse(a);
@@ -61,5 +61,36 @@ checkTrue(answer);
 answer = prompt('I love apex legends ?');
 checkTrue(answer);
 //console.log('oh yeah');
-alert('hope you enjoyed my website '+ name)
+alert('hope you enjoyed my website ' + name)
 
+let number = 1;
+
+for (let i = 1; i <= 4; i++) {
+    answer = prompt('guess a number between 0 and 20 ?')
+    if (answer == 1) {
+        alert('good job, you guessed from the ' + i + ' time');
+        break;
+    } else if (answer > 1) {
+        alert('too high');
+    } else {
+        alert('too low');
+    }
+}
+if (answer != 1) {
+    alert('the correct answer is 1');
+}
+let countries = ['USA', 'KSA', 'TURKEY', 'OMAN', 'UAE', 'SYRIA'];
+for (let i = 1; i <= 6; i++) {
+    answer = prompt('guess one of the countries I\'ve visited ?')
+    answer = answer.toUpperCase();
+    for (let j = 0; j < countries.length; j++) {
+        if (answer == countries[j]) {
+            alert('correct');
+            i = 7;
+        }
+    }
+    if (i != 7) {
+        alert('try again')
+    }
+}
+alert('the countries I\'ve visted are [ USA, KSA, TURKEY, OMAN, UAE, SYRIA ]');
